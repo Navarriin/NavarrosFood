450ce450b1cd521fc60a4d1270fe77ac6cdd0f3d
Gabriel <gabrielnavarro.vip@gmail.com>
2024-05-08 19:15:03 -0300

    Feat: Adicionando dependencias iniciais.

---
+++ src/main/java/com/navarro/food/navarrosfood/NavarrosFoodApplication.java
@@ -0,0 +1,13 @@
+package com.navarro.food.navarrosfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavarrosFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavarrosFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/navarro/food/navarrosfood/NavarrosFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navarro.food.navarrosfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavarrosFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
