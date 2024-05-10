a479c5a9d64524195a1d981a163cb039f2ab78fe
Gabriel <gabrielnavarro.vip@gmail.com>
2024-05-10 20:09:33 -0300

    Feat: Criando query customizada pelo JPA.

--- Back end/src/main/java/com/navarro/food/navarrosfood/repositories/RepositoryFood.java
+++ Back end/src/main/java/com/navarro/food/navarrosfood/repositories/RepositoryFood.java
@@ -2,6 +2,7 @@ package com.navarro.food.navarrosfood.repositories;
 
 import com.navarro.food.navarrosfood.model.FoodEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,5 +10,6 @@ import java.util.Optional;
 @Repository
 public interface RepositoryFood extends JpaRepository<FoodEntity, Long> {
 
+    @Query("SELECT f FROM FoodEntity f WHERE f.foodNumber = :id")
     Optional<FoodEntity> getFoodById(Long id);
 }
