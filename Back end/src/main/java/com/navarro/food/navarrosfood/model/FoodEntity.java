ae2ff218311df9f70c7b18265e73c36431b4569d
Gabriel <gabrielnavarro.vip@gmail.com>
2024-05-10 20:07:49 -0300

    Feat: Configurando docker compose e flyway.

--- Back end/src/main/java/com/navarro/food/navarrosfood/model/FoodEntity.java
+++ Back end/src/main/java/com/navarro/food/navarrosfood/model/FoodEntity.java
@@ -13,15 +13,17 @@ import java.math.BigDecimal;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "food")
+@Table(name = "foods")
 public class FoodEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long foodNumber;
     private String name;
     private String description;
     private String image;
+    @Column(name = "value_food")
     private BigDecimal value;
 
     public FoodEntity(String name, String description, String image, BigDecimal value) {
---
+++ Back end/src/main/resources/db/migration/V1__create-table-foods.sql
@@ -0,0 +1,7 @@
+CREATE TABLE foods (
+    id INTEGER PRIMARY KEY UNIQUE NOT NULL,
+    name VARCHAR(60) NOT NULL,
+    description VARCHAR(300) NOT NULL,
+    image TEXT NOT NULL,
+    value_food REAL NOT NULL
+);
